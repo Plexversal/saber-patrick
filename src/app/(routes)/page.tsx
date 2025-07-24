39d61435e887f20734561ee7a5e049e4fb9c25f9
Patrick Reilly <paddyjo2011@hotmail.co.uk>
2025-07-24 12:47:33 +0100

    init

---
+++ src/app/(routes)/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import styles from "@/styles/page.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
+        <div>hello</div>
+      </main>
+    </div>
+  );
+}
