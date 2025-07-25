4562f1ab42e62b601f6516b1652d187c5d76dfe7
Patrick Reilly <paddyjo2011@hotmail.co.uk>
2025-07-25 12:57:20 +0100

    added bulk styling to whole of app

--- src/components/MainSection.tsx
+++ src/components/MainSection.tsx
@@ -29,25 +29,32 @@ export default function MainSection() {
         if(matches.length == 0) return;
         if(matches.includes('No matches found')) return <div>No matches found</div>;
         let count: Record<string, number> = matches.reduce((cnt: any, cur) => (cnt[cur] = cnt[cur] + 1 || 1, cnt), {});
-        console.log(count)
-        return <div>
+        return < >
+            
             {
                 Object.entries(count).map(([key, val]) => (
-                <div key={key}>Matched term: <span>{key}</span> has <span>{val}</span> matches</div>
+                <div className={styles['matched-term-wrapper']} key={key}>Matched term: <span className={styles['matched-term']}>{key}</span> has <span>{val}</span> matches</div>
                 ))
             }
-        </div>
+        </>
     }
 
     return (
         <div className={styles['main-container']}>
+            <div className={styles['textarea-wrapper']}>
             <textarea
-                className={styles['your-textarea-class']}
+                spellCheck={false}
+                className={styles['text-area']}
                 onChange={inputOnChange}
                 value={userText}/>
                 
             <button onClick={() => setUserText(lorem.generateParagraphs(7))}>Generate Text</button>
+            </div>
+            <div className={styles['results-container']}>
+            <h2>Results</h2>
             {countInstances(workerMatches)}
+            </div>
+
         </div>
     )
 }
